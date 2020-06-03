dfc57becfc8bf204dc8becc8e8bf588a59b347d8
kenmatsu07 <rain.clear00@gmail.com>
2020-06-03 14:25:27 +0900

    react練習用

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ApiFetch from "./components/ApiFetch";
+// import Basic2 from "./components/Basic2";
+// import BasicUseEffect from "./components/BasicUseEffect";
+// import TimerContainer from "./components/TimerContainer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Basic2 /> */}
+        {/* <BasicUseEffect /> */}
+        {/* <TimerContainer /> */}
+        <ApiFetch />
       </header>
     </div>
   );
